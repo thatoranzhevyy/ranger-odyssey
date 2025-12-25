99461308ee92599f963647bbc778e2edd1c28a56
abbasqaraqoishy <thatoranzhevyy@gmail.com>
2025-12-25 21:15:21 +0500

    feat: integrate vue-tg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
   components: [{ path: '~/components', pathPrefix: false }],
-  modules: ['@nuxt/image', '@nuxt/ui', '@nuxtjs/i18n'],
+  modules: ['@nuxt/image', '@nuxt/ui', '@nuxtjs/i18n', '@nuxt/scripts'],
   css: ['~/assets/css/tailwindcss.css'],
+  app: { head: { script: [{ src: 'https://telegram.org/js/telegram-web-app.js' }] } },
   i18n: {
     locales: [
       { code: 'ru', iso: 'ru-RU', name: '🇷🇺 Русский', file: 'ru.json' },
