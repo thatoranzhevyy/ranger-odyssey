d20b8173356689d160e052bc30fad9ad198c2490
abbasqaraqoishy <thatoranzhevyy@gmail.com>
2025-12-28 18:48:06 +0500

    скрыт app bar

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -61,7 +61,7 @@ const days = [
 </script>
 
 <template>
-  <div class="bg-elevated/30 flex h-14 items-center justify-between p-2">
+  <div class="hidden bg-elevated/30 flex h-14 items-center justify-between p-2">
     <UButton size="xl" variant="ghost" color="neutral" icon="i-ph-caret-left-bold" />
     <div
       class="text-[28px] leading-none font-extrabold tracking-[0.14em] text-white uppercase italic"
