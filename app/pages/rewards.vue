e62b824c40930cb0a99ecae38aa466f87d70be1f
abbasqaraqoishy <thatoranzhevyy@gmail.com>
2026-01-10 01:39:43 +0500

    Update 10.01.2026

--- app/pages/rewards.vue
+++ app/pages/rewards.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $api } = useNuxtApp();
+
+const users = ref([]);
+
+onMounted(async () => {
+  users.value = await $api('/api/v1/profile');
+});
+</script>
 
 <template>
   <div class="min-h-[calc(100vh-var(--ui-header-height))] p-2">
+    <pre>{{ users }}</pre>
     <Reward />
   </div>
 </template>
